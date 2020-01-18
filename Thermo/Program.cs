c5361f3c8f48eef608df039b43fed23ce6061095
Johan Smarius <jsmarius@hotmail.com>
2020-01-18 19:56:14 +0100

    Added partial client code

---
+++ Thermo/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+using System;
+using static SynchronisationService.Protos.SynchronisationService;
+
+namespace Thermo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5005");
+
+            var client = new SynchronisationServiceClient(channel);
+
+
+
+
+        }
+    }
+}
