af5051fd7372f9624491e48eed4f336c8025f014
Johan Smarius <jsmarius@hotmail.com>
2020-01-18 19:49:57 +0100

    Added temp service

---
+++ SynchronisationService/Services/SynchronisationService.cs
@@ -0,0 +1,30 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using SynchronisationService.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SynchronisationService.Services
+{
+    public class SynchronisationService : Protos.SynchronisationService.SynchronisationServiceBase
+    {
+        public SynchronisationService()
+        {
+
+        }
+
+        public override async Task<Empty> AddTemperatureReading(IAsyncStreamReader<AddTemperatureReadingRequest> requestStream, 
+            ServerCallContext context)
+        {
+            await foreach (var reading in requestStream.ReadAllAsync())
+            {
+                Console.WriteLine($"Received @ {reading.RegisteredAt.ToDateTime()} temp {reading.Temperature}");
+            }
+
+            return new Empty();
+
+        }
+    }
+}
