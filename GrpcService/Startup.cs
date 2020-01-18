e64b8617fb6b38c9b24784ec7606288ff4069622
Johan Smarius <jsmarius@hotmail.com>
2020-01-18 15:11:08 +0100

    Empty service

--- GrpcService/Startup.cs
+++ GrpcService/Startup.cs
@@ -16,7 +16,7 @@ namespace GrpcService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddGrpc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,8 +31,6 @@ namespace GrpcService
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
-
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
