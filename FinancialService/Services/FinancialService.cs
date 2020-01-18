4e7331e8d7a70e9bffc1f4ca0921407e3495fff3
Johan Smarius <jsmarius@hotmail.com>
2020-01-18 19:14:43 +0100

    Added financial service

---
+++ FinancialService/Services/FinancialService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static FinancialService.FinancialService;
+
+namespace FinancialService.Services
+{
+    public class FinancialService : FinancialServiceBase
+    {
+        private readonly ILogger<FinancialService> _logger;
+
+        public FinancialService(ILogger<FinancialService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<AddInvoiceResponse> AddInvoice(AddInvoiceRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new AddInvoiceResponse());
+        }
+    }
+}
