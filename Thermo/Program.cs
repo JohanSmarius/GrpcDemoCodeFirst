62ef77fdfe7c4928bb1ac9f5a6a211693984fe94
Johan Smarius <jsmarius@hotmail.com>
2020-01-18 21:11:03 +0100

    Updated packages Created thermo client

--- Thermo/Program.cs
+++ Thermo/Program.cs
@@ -1,12 +1,16 @@
 ﻿using Grpc.Net.Client;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using SynchronisationService.Protos;
 using static SynchronisationService.Protos.SynchronisationService;
 
 namespace Thermo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
@@ -14,9 +18,29 @@ namespace Thermo
 
             var client = new SynchronisationServiceClient(channel);
 
+            using (var call = client.AddTemperatureReading())
+            {
+                var timer = new System.Timers.Timer();
+                var randomizer = new Random(Int32.MaxValue);
+                timer.AutoReset = true;
+                timer.Elapsed += (o, args) =>
+                {
+                    var request = new AddTemperatureReadingRequest()
+                    {
+                        RegisteredAt = Timestamp.FromDateTime(DateTime.UtcNow),
+                        Temperature = 20.00 * randomizer.NextDouble()
 
+                    };
 
+                    call.RequestStream.WriteAsync(request);
+                };
 
+                timer.Start();
+
+                var shouldEnd = Console.ReadLine();
+
+                await call.RequestStream.CompleteAsync();
+            }
         }
     }
 }
